eb1a290b21f8d8875e7664e27fa11b64c490aad4
Hugo Valk <huvalk@ttu.ee>
2020-03-29 18:01:20 +0300

    48

---
+++ Facade/Quantity/MeasureTermView.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Abc.Data.Quantity;
+using Abc.Facade.Common;
+
+namespace Abc.Facade.Quantity
+{
+    public sealed class MeasureTermView :CommonTermView
+    {
+        [Required]
+        [DisplayName("Measure")]
+        public string MasterId { get; set; }
+    }
+}
---
+++ Tests/Facade/Quantity/MeasureTermViewTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abc.Facade.Common;
+using Abc.Facade.Quantity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Abc.Tests.Facade.Quantity
+{
+    [TestClass]
+    public class MeasureTermViewTests : SealedClassTests<MeasureTermView, CommonTermView>
+    {
+        [TestMethod]
+        public void MasterIdTest() => isNullableProperty(() => obj.MasterId, x => obj.MasterId = x);
+
+    }
+
+
+}
+
+
