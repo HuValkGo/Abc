50e7a7ae1baac01041bc6e2bcb41401b10127738
Hugo Valk <huvalk@ttu.ee>
2020-03-29 11:41:55 +0300

    45

---
+++ Data/Quantity/CommonTerm.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Abc.Data.Common;
+
+namespace Abc.Data.Quantity
+{
+    public class CommonTerm:PeriodData
+    {
+         public string MasterId { get; set; }
+         public string TermId { get; set; }
+         public string Power { get; set; }
+    }
+}
