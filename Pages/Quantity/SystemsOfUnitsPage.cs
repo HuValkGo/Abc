54ae93d06270d165bb0666326c1e3cbcf08974a1
Hugo Valk <huvalk@ttu.ee>
2020-04-09 16:11:49 +0300

    59

--- Pages/Quantity/SystemsOfUnitsPage.cs
+++ Pages/Quantity/SystemsOfUnitsPage.cs
@@ -10,12 +10,12 @@ namespace Abc.Pages.Quantity
     {
         protected internal SystemsOfUnitsPage(ISystemsOfUnitsRepository r) : base(r)
         {
-            PageTitle = "Measure Terms";
+            PageTitle = "Systems Of Units";
         }
 
         public override string ItemId => Item.Id;
 
-        protected internal override string getPageUrl() => "/Quantity/SystemOfUnits";
+        protected internal override string getPageUrl() => "/Quantity/SystemsOfUnits";
 
         protected internal override SystemOfUnits toObject(SystemOfUnitsView view)
         {
